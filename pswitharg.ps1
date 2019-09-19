93aee7d3f5750da47235e68106d612fe0fba5c41
atulrajwar <33245723+atulrajwar@users.noreply.github.com>
2019-09-19 18:09:44 +0530

    pw login file committed

---
+++ pswitharg.ps1
@@ -0,0 +1,14 @@
+﻿param ( 
+[string]$applicationId,
+[string]$pass,
+[string]$tenantid
+)
+
+#Write-Host $pass
+$password=$pass|ConvertTo-SecureString -AsPlainText -Force
+$Cred = New-Object System.Management.Automation.PsCredential($applicationId,$password)
+Connect-AzAccount -ServicePrincipal -Credential $Cred -Tenant $tenantid
+##param( [String] $test, $test1)
+
+#Write-Host ($password)
+#Write-Host ($tenantid)
